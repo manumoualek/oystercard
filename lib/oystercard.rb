93b5a9c8a5c88e55b648ff99d4c5d51e324c2331
manu <manuelmoualek@gmail.com>
2021-07-26 15:50:12 +0100

    Test for max balance + added top-up limit in top_up method + max constant

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,14 @@
 class Oystercard
   attr_reader :balance
+  MAX_BALANCE = 90
+
   def initialize
     @balance = 0
+    # @max_balance = 90
+  end
+  
+  def top_up(amount)
+    fail "Can't top up balance over 90" unless @balance + amount <= MAX_BALANCE
+      @balance += amount
   end  
 end  
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,17 @@ describe Oystercard do
       expect(subject.balance).to eql 0
     end
   end
+
+  describe "#top_up" do
+    it "tops up card with amount requested/passed in as argument" do
+      expect {subject.top_up 5 }.to change {subject.balance}.by 5
+    end
+
+    it "throws an error when top up takes balance over 90" do
+      subject.top_up(Oystercard::MAX_BALANCE)
+      expect{subject.top_up(1)}.to raise_error("Can't top up balance over 90")
+    end  
+  end
+
+
 end
